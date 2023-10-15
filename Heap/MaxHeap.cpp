743b73d41b241cbf41edb5c7cdc83cda298568cb
someshrawat00 <someshrawat00@gmail.com>
2023-10-15 18:13:22 +0530

    HeapSort

--- Heap/MaxHeap.cpp
+++ Heap/MaxHeap.cpp
@@ -79,17 +79,58 @@ public:
     }
 };
 
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
+
+    if (left < n && arr[largest] < arr[left])
+    {
+        largest = left;
+    }
+
+    if (right < n && arr[largest] < arr[right])
+    {
+        largest = right;
+    }
+
+    if (largest != i)
+    {
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+
+void print(int arr[], int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
     Heap h;
-    h.insert(50);
-    h.insert(55);
-    h.insert(53);
-    h.insert(52);
-    h.insert(54);
-    h.print();
-    h.del();
-    h.del();
-    h.print();
+    // h.insert(50);
+    // h.insert(55);
+    // h.insert(53);
+    // h.insert(52);
+    // h.insert(54);
+    // h.print();
+    // h.del();
+    // h.del();
+
+    int arr[6] = {-1, 54, 53, 55, 52, 50};
+    int n = 5;
+
+    for (int i = n/2; i > 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+
+    print(arr, 5);
     return 0;
 }
